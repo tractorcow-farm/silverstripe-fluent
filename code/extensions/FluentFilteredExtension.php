1ad3e37141cbaf558711790ffce423a385b02631
Damian Mooyman <damian@silverstripe.com>
2014-08-08 22:48:09 +1200

    BUG Fix duplicate fields appearing Fixes #85

--- code/extensions/FluentFilteredExtension.php
+++ code/extensions/FluentFilteredExtension.php
@@ -2,15 +2,15 @@
 
 /**
  * Data extension class for a class which should only be present in one or more locales
- * 
+ *
  * @package fluent
  * @author Damian Mooyman <damian.mooyman@gmail.com>
  */
 class FluentFilteredExtension extends DataExtension {
-	
+
 	/**
 	 * Set the filter of locales to the specified locale, or array of locales
-	 * 
+	 *
 	 * @param string|array $locale Locale, or list of locales
 	 * @param string $locale... Additional locales
 	 */
@@ -21,10 +21,10 @@ class FluentFilteredExtension extends DataExtension {
 			$this->owner->$field = in_array($locale, $locales);
 		}
 	}
-	
+
 	/**
 	 * Gets the list of locales this items is filtered against
-	 * 
+	 *
 	 * @param boolean $visible Set to false to get excluded instead of included locales
 	 * @return array List of locales
 	 */
@@ -37,10 +37,10 @@ class FluentFilteredExtension extends DataExtension {
 		}
 		return $locales;
 	}
-	
+
 	/**
 	 * Determine if this object is visible (or excluded) in the specified locale
-	 * 
+	 *
 	 * @param string $locale Locale to check against
 	 * @return boolean True if the object is visible in the specified locale
 	 */
@@ -48,28 +48,27 @@ class FluentFilteredExtension extends DataExtension {
 		$field = Fluent::db_field_for_locale("LocaleFilter", $locale);
 		return $this->owner->$field;
 	}
-	
+
 	public static function get_extra_config($class, $extension, $args) {
-		
+
 		// Create a separate boolean field to indicate visibility in each field
 		$db = array();
 		$defaults = array();
-		
+
 		foreach(Fluent::locales() as $locale) {
 			$field = Fluent::db_field_for_locale("LocaleFilter", $locale);
 			// Copy field to translated field
 			$db[$field] = 'Boolean(1)';
 			$defaults[$field] = '1';
 		}
-		
+
 		return array(
 			'db' => $db,
 			'defaults' => $defaults
 		);
 	}
-	
-	public function updateCMSFields(FieldList $fields) {
 
+	public function updateCMSFields(FieldList $fields) {
 		// Present a set of checkboxes for filtering this item by locale
 		$filterField = FieldGroup::create()
 			->setTitle(
@@ -78,9 +77,10 @@ class FluentFilteredExtension extends DataExtension {
 			->setDescription(
 				_t('Fluent.LocaleFilterDescription', 'Check a locale to show this item on that locale')
 			);
-		
+
 		foreach(Fluent::locales() as $locale) {
 			$id = Fluent::db_field_for_locale("LocaleFilter", $locale);
+			$fields->removeByName($id, true); // Remove existing (in case it was auto scaffolded)
 			$title = i18n::get_locale_name($locale);
 			$filterField->push(new CheckboxField($id, $title));
 		}
@@ -92,7 +92,7 @@ class FluentFilteredExtension extends DataExtension {
 			$fields->add($filterField);
 		}
 	}
-	
+
 	/**
 	 * Amend freshly created DataQuery objects with the current locale and frontend status
 	 *
@@ -103,17 +103,17 @@ class FluentFilteredExtension extends DataExtension {
 		$dataQuery->setQueryParam('Fluent.Locale', Fluent::current_locale());
 		$dataQuery->setQueryParam('Fluent.IsFrontend', Fluent::is_frontend());
 	}
-	
+
 	public function augmentSQL(SQLQuery &$query, DataQuery &$dataQuery = null) {
-		
+
 		// Skip ID based filters
 		if($query->filtersOnID()) return;
-		
+
 		// Skip filter in the CMS
 		$isFrontend = $dataQuery->getQueryParam('Fluent.IsFrontend');
 		if($isFrontend === null) $isFrontend = Fluent::is_frontend();
 		if(!$isFrontend) return;
-		
+
 		// Add filter for locale
 		$locale = $dataQuery->getQueryParam('Fluent.Locale') ?: Fluent::current_locale();
 		$query->addWhere("\"$this->ownerBaseClass\".\"LocaleFilter_{$locale}\" = 1");
@@ -121,7 +121,7 @@ class FluentFilteredExtension extends DataExtension {
 
 	/**
 	 * Add 'Hidden' flag to the SiteTree object if the page is not present in this locale
-	 * 
+	 *
 	 * @param array $flags
 	 */
 	public function updateStatusFlags(&$flags) {
