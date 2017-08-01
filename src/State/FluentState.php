e9d6b8c219f7a8da8aa4fd238ccf33584aaab9fd
Robbie Averill <robbie@silverstripe.com>
2017-08-01 17:19:32 +1200

    NEW Add FluentMiddleware to register FluentState as a service

--- src/State/FluentState.php
+++ src/State/FluentState.php
@@ -2,6 +2,7 @@
 
 namespace TractorCow\Fluent\State;
 
+use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injectable;
 
 /**
@@ -18,14 +19,54 @@ class FluentState
      */
     protected $locale;
 
+    /**
+     * Current request object
+     *
+     * @var HTTPRequest
+     */
+    protected $request;
+
+    /**
+     * Get the currently active locale code
+     *
+     * @return string
+     */
     public function getLocale()
     {
         return $this->locale;
     }
 
+    /**
+     * Set the currently active locale code
+     *
+     * @param string $locale
+     * @return $this
+     */
     public function setLocale($locale)
     {
         $this->locale = $locale;
+        return $this;
+    }
+
+    /**
+     * Get the currently active request
+     *
+     * @return HTTPRequest
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Set the active request
+     *
+     * @param HTTPRequest $request
+     * @return $this
+     */
+    public function setRequest(HTTPRequest $request)
+    {
+        $this->request = $request;
         return $this;
     }
 }
