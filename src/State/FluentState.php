b3c5aea41c53c34103228f5c762c48baf2b6dfa7
Damian Mooyman <damian@silverstripe.com>
2017-08-25 13:32:25 +1200

    Add state sandbox closure feature

--- src/State/FluentState.php
+++ src/State/FluentState.php
@@ -3,6 +3,7 @@
 namespace TractorCow\Fluent\State;
 
 use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\Core\Injector\Injector;
 
 /**
  * Stores the current fluent state
@@ -125,5 +126,23 @@ class FluentState
     {
         $this->isFrontend = $isFrontend;
         return $this;
+    }
+
+    /**
+     * Perform the given operation in an isolated state.
+     * On return, the state will be restored, so any modifications are temporary.
+     *
+     * @param callable $callback Callback to run. Will be passed the nested state as a parameter
+     * @return mixed Result of callback
+     */
+    public function withState($callback)
+    {
+        $newState = clone $this;
+        try {
+            Injector::inst()->registerService($newState);
+            return $callback($newState);
+        } finally {
+            Injector::inst()->registerService($this);
+        }
     }
 }
