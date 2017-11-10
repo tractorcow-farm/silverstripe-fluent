cfb28b862b0729b67a816bfe39ee0b1e5a1e6c6c
Robbie Averill <robbie@silverstripe.com>
2017-11-10 16:07:33 +1300

    FIX Do not treat CMS preview requests as frontend

--- tests/php/Middleware/InitStateMiddlewareTest.php
+++ tests/php/Middleware/InitStateMiddlewareTest.php
@@ -11,11 +11,12 @@ class InitStateMiddlewareTest extends SapphireTest
     /**
      * @dataProvider isFrontendProvider
      * @param string $url
+     * @param array $getVars
      * @param string $expected
      */
-    public function testGetIsFrontend($url, $expected)
+    public function testGetIsFrontend($url, $getVars, $expected)
     {
-        $request = new HTTPRequest('GET', $url);
+        $request = new HTTPRequest('GET', $url, $getVars);
         $result = (new InitStateMiddleware)->getIsFrontend($request);
         $this->assertSame($expected, $result, 'isFrontend detects whether a request is for the frontend website');
     }
@@ -26,13 +27,14 @@ class InitStateMiddlewareTest extends SapphireTest
     public function isFrontendProvider()
     {
         return [
-            ['admin', false],
-            ['admin/', false],
-            ['dev/build', false],
-            ['graphql', false],
-            ['/', true],
-            ['foo', true],
-            ['my-blog/my-post', true],
+            ['admin', [], false],
+            ['admin/', [], false],
+            ['dev/build', [], false],
+            ['graphql', [], false],
+            ['/', [], true],
+            ['foo', [], true],
+            ['my-blog/my-post', [], true],
+            ['my-blog/my-post', ['CMSPreview' => 1], false],
         ];
     }
 }
