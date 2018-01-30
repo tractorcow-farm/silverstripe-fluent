1d7cdb2d08b6e5f76ad8b2479070709d181baa85
guy van bael <guy.van.bael@gmail.com>
2018-01-30 11:06:43 +0100

    Create FluentSitemapController.php

---
+++ src/Control/FluentSitemapController.php
@@ -0,0 +1,22 @@
+<?php
+namespace TractorCow\Fluent\Control;
+use Wilr\GoogleSitemaps\Control\GoogleSitemapController;
+
+
+    /**
+     * Provides extensions for multilingual views of pages in sitemap.xml
+     * @link https://support.google.com/webmasters/answer/2620865?hl=en&ref_topic=2370587
+     *
+     * @see GoogleSitemapController
+     * @package fluent
+     * @author Damian Mooyman <damian.mooyman@gmail.com>
+     */
+    class FluentSitemapController extends GoogleSitemapController
+    {
+        public function init()
+        {
+            parent::init();
+            // Reset any session locale to use the default locale as the standard 'base' locale
+            //Fluent::set_persist_locale(Fluent::default_locale(true));
+        }
+    }
