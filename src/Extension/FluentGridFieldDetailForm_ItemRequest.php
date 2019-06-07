3ddfca4b34d9de577d4e32dbee891eb114c96c89
Nivanka Fonseka <nivanka@silverstripers.com>
2019-06-07 19:19:04 +0530

    added the locale for the CMS fields

--- src/Extension/FluentGridFieldDetailForm_ItemRequest.php
+++ src/Extension/FluentGridFieldDetailForm_ItemRequest.php
@@ -1,28 +1,33 @@
 <?php
+/**
+ * Created by Nivanka Fonseka (nivanka@silverstripers.com).
+ * User: nivankafonseka
+ * Date: 6/7/19
+ * Time: 7:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
 
 namespace TractorCow\Fluent\Extension;
 
-use SilverStripe\Core\Extension;
+
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\HiddenField;
-use SilverStripe\View\Requirements;
+use SilverStripe\ORM\DataExtension;
 use TractorCow\Fluent\State\FluentState;
 
-class FluentLeftAndMainExtension extends Extension
+class FluentGridFieldDetailForm_ItemRequest extends DataExtension
 {
-    public function init()
-    {
-        Requirements::javascript("tractorcow/silverstripe-fluent:client/dist/js/fluent.js");
-        Requirements::css("tractorcow/silverstripe-fluent:client/dist/styles/fluent.css");
-    }
 
-    public function updateEditForm(Form $form)
+    public function updateItemEditForm(Form $form)
     {
+
         /* @var $state FluentState */
         $state = Injector::inst()->get(FluentState::class);
         $locale = $state->getLocale();
         $form->Fields()->push(HiddenField::create('l')->setValue($locale));
+
     }
+
 
 }
