71f18e670a5395bb965b4ee920ca797924c41529
Werner M. Krauß <werner.krauss@netwerkstatt.at>
2019-07-11 13:36:07 +0200

    get test demodata in DB

---
+++ src/Dev/TranslatedDataObject.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace TractorCow\Fluent\Dev;
+
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DB;
+use SilverStripe\ORM\Queries\SQLUpdate;
+
+/**
+ * Dataobject to test migration of old SS3 data to SS4
+ *
+ * Assuming we translated to de_AT and en_US
+ *
+ *
+ * Class TranslatedDataObject
+ * @package TractorCow\Fluent\Dev
+ */
+class TranslatedDataObject extends DataObject implements TestOnly
+{
+    private static $db = [
+        'Title' => 'Varchar',
+        'Name' => 'Varchar'
+    ];
+
+    private static $translate = [
+        'Title',
+        'Name'
+    ];
+
+    private static $table_name = 'FluentTestDataObject';
+
+    /**
+     * @var array
+     */
+    private static $old_fluent_fields = [
+        'Title_en_US',
+        'Title_de_AT',
+        'Name_en_US',
+        'Name_de_AT'
+    ];
+
+    public function onAfterWrite()
+    {
+        $update = SQLUpdate::create()
+            ->setTable($this->baseTable());
+
+        foreach (self::config()->get('old_fluent_fields') as $field) {
+            $update->assign($field, $this->getField($field)); //value is set from fixtures in $this->record
+            $update->execute();
+        }
+    }
+
+
+    /**
+     * Helper to get the old *_translationgroups table and the Locale field created
+     */
+    public function requireTable()
+    {
+        parent::requireTable();
+
+        $baseDataClass = DataObject::getSchema()->baseDataClass($this->ClassName);
+        if ($this->ClassName != $baseDataClass) {
+            return;
+        }
+
+        $schemaManager = DB::get_schema();
+
+        foreach (self::config()->get('old_fluent_fields') as $field) {
+            $schemaManager->requireField($this->baseTable(), $field, 'VARCHAR(255) NULL DEFAULT NULL ');
+        }
+    }
+}
---
+++ tests/php/Task/MigrateFluentTaskTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace TractorCow\Fluent\Tests\Task;
+
+
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\Queries\SQLSelect;
+use TractorCow\Fluent\Dev\TranslatedDataObject;
+
+class MigrateFluentTaskTest extends SapphireTest
+{
+    protected static $fixture_file = 'MigrateFluentTaskTest.yml';
+
+    protected static $extra_dataobjects = [
+        TranslatedDataObject::class
+    ];
+
+    public function testFixturesAreSetupWithOldData()
+    {
+        $house = $this->objFromFixture(TranslatedDataObject::class, 'house');
+
+        $id = $house->ID;
+
+        $allFields = SQLSelect::create()
+            ->setFrom(Config::inst()->get(TranslatedDataObject::class, 'table_name'))
+            ->addWhere('ID = ' . $id)
+            ->firstRow()
+            ->execute();
+        $record = $allFields->record();
+
+        $this->assertEquals('A House', $record['Title_en_US']);
+        $this->assertEquals('Something', $record['Name_en_US']);
+        $this->assertEquals('Ein Haus', $record['Title_de_AT']);
+        $this->assertEquals('Irgendwas', $record['Name_de_AT']);
+    }
+}
