42e999a7f4d7bb454f555af09715ea24bce7829c
Damian Mooyman <damian.mooyman@isobar.com>
2020-05-28 09:54:36 +1200

    BUG Fix compatibility issues with mysql 5.7 and above Fixes #257

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
 use SilverStripe\Dev\Deprecation;
+use SilverStripe\ORM\DB;
 
 Deprecation::notification_version('4.0.0', 'tractorcow/silverstripe-fluent');
+
+// Credit to https://github.com/sunnysideup thank you for the mysql fix
+DB::query("SET SESSION sql_mode='REAL_AS_FLOAT,PIPES_AS_CONCAT,ANSI_QUOTES,IGNORE_SPACE';");
