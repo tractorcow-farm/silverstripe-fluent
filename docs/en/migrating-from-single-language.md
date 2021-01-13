2b6ca3f6a6644791588a85ecaefa292cb6f7a5a4
Mojmir Fendek <mfendek@silverstripe.com>
2021-01-14 12:11:24 +1300

    NEW: Initial page localisation task (for single language sites import).
    (cherry picked from commit dfd8b4b32d80964565fdfd6de42f6e9d06d937b1)

--- docs/en/migrating-from-single-language.md
+++ docs/en/migrating-from-single-language.md
@@ -38,3 +38,27 @@ When you run `dev/build?flush` again, this adds the records to the database if t
 Now your site is broken, cause no pages have been published and added as translated page in your default locale. 
 You can either publish all pages manually or use [publishall](https://docs.silverstripe.org/en/4/developer_guides/debugging/url_variable_tools/#building-and-publishing-urls) to publish all pages in bulk.
 If you run `/admin/pages/publishall` in your browser  your site will be fixed again and you can start adding translated content.  
+
+### Automated tools for localisation
+
+`InitialPageLocalisation` dev task can be used to either only localise or localise & publish your pages.
+This dev task can be run either via CLI or queued as a job if Queued jobs module is installed.
+
+Localise only example
+
+```
+dev/tasks/initial-page-localisation-task
+```
+
+Localise & publish example
+
+```
+dev/tasks/initial-page-localisation-task publish=1
+```
+
+Localisation in batches can be done by using the `limit` option.
+Example below will localise & publish five pages on each run.
+
+```
+dev/tasks/initial-page-localisation-task publish=1&limit=5
+```
---
+++ src/Task/InitialPageLocalisationTask.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace TractorCow\Fluent\Task;
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Versioned\Versioned;
+use TractorCow\Fluent\Extension\FluentSiteTreeExtension;
+use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
+
+class InitialPageLocalisationTask extends BuildTask
+{
+    /**
+     * @var string
+     */
+    private static $segment = 'initial-page-localisation-task';
+
+    /**
+     * @var string
+     */
+    protected $title = 'Initial page localisation';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Intended for projects which already have some pages when Fluent module is added.' .
+    ' This dev task will localise / publish all pages in the default locale. Locale setup has to be done before running this task.' .
+    ' Pages which are not published will not be published, only localised. Pages which are already localised will be skipped.';
+
+    /**
+     * @param HTTPRequest $request
+     */
+    public function run($request)
+    {
+        $publish = (bool) $request->getVar('publish');
+        $limit = (int) $request->getVar('limit');
+
+        /** @var Locale $globalLocale */
+        $globalLocale = Locale::get()
+            ->filter(['IsGlobalDefault' => 1])
+            ->sort('ID', 'ASC')
+            ->first();
+
+        if (!$globalLocale) {
+            echo 'Please set global locale first!' . PHP_EOL;
+
+            return;
+        }
+
+        $pageIds = FluentState::singleton()->withState(static function (FluentState $state) use ($limit): array {
+            $state->setLocale(null);
+            $pages = SiteTree::get()->sort('ID', 'ASC');
+
+            if ($limit > 0) {
+                $pages = $pages->limit($limit);
+            }
+
+            return $pages->column('ID');
+        });
+
+        $localised = FluentState::singleton()->withState(
+            static function (FluentState $state) use ($globalLocale, $pageIds, $publish): int {
+                $state->setLocale($globalLocale->Locale);
+                $localised = 0;
+
+                foreach ($pageIds as $pageId) {
+                    /** @var SiteTree|FluentSiteTreeExtension $page */
+                    $page = SiteTree::get()->byID($pageId);
+
+                    if ($page->isDraftedInLocale()) {
+                        continue;
+                    }
+
+                    $page->writeToStage(Versioned::DRAFT);
+                    $localised += 1;
+
+                    if (!$publish) {
+                        continue;
+                    }
+
+                    // Check if the base record was published - if not then we don't need to publish
+                    // as this would leak draft content, we only want to publish pages which were published
+                    // before Fluent module was added
+                    $pageId = $page->ID;
+                    $isBaseRecordPublished = FluentState::singleton()->withState(
+                        static function (FluentState $state) use ($pageId): bool {
+                            $state->setLocale(null);
+                            $page = SiteTree::get_by_id($pageId);
+
+                            if ($page === null) {
+                                return false;
+                            }
+
+                            return $page->isPublished();
+                        }
+                    );
+
+                    if (!$isBaseRecordPublished) {
+                        continue;
+                    }
+
+                    $page->publishRecursive();
+                }
+
+                return $localised;
+            }
+        );
+
+        echo sprintf('Localised %d pages.', $localised) . PHP_EOL;
+    }
+}
