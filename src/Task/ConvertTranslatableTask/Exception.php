3dbdb254acdd54a4d5a1e1d150b65148696e6b7f
Guy Sartorelli <guy.sartorelli@gmail.com>
2024-09-10 15:24:04 +1200

    API Deprecate API that will be removed

--- src/Task/ConvertTranslatableTask/Exception.php
+++ src/Task/ConvertTranslatableTask/Exception.php
@@ -2,7 +2,22 @@
 
 namespace TractorCow\Fluent\Task\ConvertTranslatableTask;
 
+use SilverStripe\Dev\Deprecation;
+
+/**
+ * @deprecated 7.3.0 Will be removed without equivalent functionality to replace it
+ */
 class Exception extends \Exception
 {
-
+    public function __construct()
+    {
+        parent::__construct();
+        Deprecation::withNoReplacement(function () {
+            Deprecation::notice(
+                '7.3.0',
+                'Will be removed without equivalent functionality to replace it',
+                Deprecation::SCOPE_CLASS
+            );
+        });
+    }
 }
